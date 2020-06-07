8b3a16c0837aeab7708d5e08c9da017db99166b6
Paulo Gasetta <pgasetta@gmail.com>
2020-06-07 02:24:40 -0300

    Teste GIT

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -16,7 +16,6 @@ export default function App() {
  if(!fontsLoaded){
    return <AppLoading />
  }
-
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
